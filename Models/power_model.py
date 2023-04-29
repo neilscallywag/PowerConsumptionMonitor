23ba06e633fe22c3775f25b7b0ab3ad4701ac235
Neil <64523806+neilscallywag@users.noreply.github.com>
2023-04-30 02:53:45 +0800

    bug fixes

--- Models/power_model.py
+++ Models/power_model.py
@@ -78,7 +78,7 @@ class PowerModel:
         capacitor_energy = self.capacitor_energy(capacitive_load, self.supply_voltage)
 
         # Increase the duration for better representation of the average activity factor
-        duration = 10 
+        duration = 5
         interval = 1
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -104,17 +104,26 @@ class PowerModel:
     def capacitor_energy(self, capacitance, voltage):
         return 0.5 * capacitance * voltage * voltage
 
+
     def activity_factor_cpu(self, tup):
+        # The issue here is that the CPU activity unless it is a resource heavy application, is always 0 due to accuracy of the library
+
         process, interval, duration = tup[0], tup[1], tup[2]
         cpu_percentages = []
+
+        process.cpu_percent(interval=None)
+
         for _ in range(int(duration / interval)):
-            cpu_percentages.append(process.cpu_percent(interval=interval))
-            time.sleep(interval)
-        print("CPU percentage: ", cpu_percentages)
+            cpu_percent = process.cpu_percent(interval=interval)  
+            cpu_percentages.append(cpu_percent)
+            print("CPU percentage: ", cpu_percent)
+
+        print("CPU percentage (list): ", cpu_percentages)
         cpu_percent_end = process.cpu_percent(interval=None)
         activity_factor = abs((cpu_percent_end - np.mean(cpu_percentages))) / 100.0
         print("CPU AF: ", activity_factor)
         return activity_factor
+
 
     def activity_factor_gpu(self, args):
         #Previous implementation was doing something similar to the CPu activity factor method.
@@ -124,9 +133,13 @@ class PowerModel:
         # Another issue is that in linux, for some reason the library fails to detect GPU. It could simply just be an error
         # On my part in failing to set up my Virtual Machine properly. However, as it stands, i do not have a solution for this.
         gpu_list = GPUtil.getGPUs()
+        print(gpu_list)
         if len(gpu_list) >0:
             for gpu in gpu_list:
+                print(gpu)
+                print(gpu.load)
                 return gpu.load
+            
         return 0.0
 
 
